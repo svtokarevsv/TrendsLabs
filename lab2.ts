720b6360a743d3401948f347f426883c6c9a5daa
Sviatoslav Tokariev <svtokarevsv@gmail.com>
2018-05-18 13:35:53 -0400

    add extra line breaks for beauty

--- lab2.ts
+++ lab2.ts
@@ -25,13 +25,15 @@ enum months{
 let todayDate = document.getElementById('p--today-date')
 let today:Date=new Date()
 todayDate.innerHTML=`Today is ${days[today.getDay()]} ${months[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}`
+
 document.getElementById('button--birthday').addEventListener('click',function (e) {
     let birthDate = document.getElementById('input--date-picker')as HTMLInputElement
     if(birthDate.value){
-        let bDate = parseDate(birthDate.value)
+        let bDate:Date = parseDate(birthDate.value)
         document.getElementById('p--birthday-message').innerHTML=getBirthDayMessage(bDate)
     }
 })
+
 function getBirthDayMessage(birthDate:Date):string{
     let today:Date = new Date()
     if(today.getMonth()===birthDate.getMonth()&&today.getDate()===birthDate.getDate()){
@@ -43,6 +45,7 @@ function getBirthDayMessage(birthDate:Date):string{
     }
     return `Your birthday will be on ${days[nextBirthDate.getDay()]} ${months[nextBirthDate.getMonth()]} ${nextBirthDate.getDate()}, ${nextBirthDate.getFullYear()}`
 }
+
 function parseDate(str):Date {
     const mdy = str.split('-');
     return new Date(mdy[0], mdy[1]-1, mdy[2]);

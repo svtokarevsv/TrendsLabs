e803ce7408f5cffacfc9bdbd55e69fe8c6336693
Sviatoslav Tokariev <svtokarevsv@gmail.com>
2018-05-18 13:04:01 -0400

    lab2 part 2

--- lab2.js
+++ lab2.js
@@ -25,4 +25,26 @@ var months;
 })(months || (months = {}));
 let todayDate = document.getElementById('p--today-date');
 let today = new Date();
-todayDate.innerHTML = `Today is ${days[today.getDay()]} ${months[today.getMonth()]}, ${today.getFullYear()}`;
+todayDate.innerHTML = `Today is ${days[today.getDay()]} ${months[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}`;
+document.getElementById('button--birthday').addEventListener('click', function (e) {
+    let birthDate = document.getElementById('input--date-picker');
+    if (birthDate.value) {
+        let bDate = parseDate(birthDate.value);
+        document.getElementById('p--birthday-message').innerHTML = getBirthDayMessage(bDate);
+    }
+});
+function getBirthDayMessage(birthDate) {
+    let today = new Date();
+    if (today.getMonth() === birthDate.getMonth() && today.getDate() === birthDate.getDate()) {
+        return 'Happy birthday!';
+    }
+    let nextBirthDate = new Date(today.getFullYear(), birthDate.getMonth(), birthDate.getDate());
+    if (nextBirthDate < today) {
+        nextBirthDate = new Date(today.getFullYear() + 1, birthDate.getMonth(), birthDate.getDate());
+    }
+    return `Your birthday will be on ${days[nextBirthDate.getDay()]} ${months[nextBirthDate.getMonth()]} ${nextBirthDate.getDate()}, ${nextBirthDate.getFullYear()}`;
+}
+function parseDate(str) {
+    const mdy = str.split('-');
+    return new Date(mdy[0], mdy[1] - 1, mdy[2]);
+}
--- lab2.ts
+++ lab2.ts
@@ -24,4 +24,26 @@ enum months{
 
 let todayDate = document.getElementById('p--today-date')
 let today:Date=new Date()
-todayDate.innerHTML=`Today is ${days[today.getDay()]} ${months[today.getMonth()]}, ${today.getFullYear()}`
+todayDate.innerHTML=`Today is ${days[today.getDay()]} ${months[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}`
+document.getElementById('button--birthday').addEventListener('click',function (e) {
+    let birthDate = document.getElementById('input--date-picker')as HTMLInputElement
+    if(birthDate.value){
+        let bDate = parseDate(birthDate.value)
+        document.getElementById('p--birthday-message').innerHTML=getBirthDayMessage(bDate)
+    }
+})
+function getBirthDayMessage(birthDate:Date){
+    let today:Date = new Date()
+    if(today.getMonth()===birthDate.getMonth()&&today.getDate()===birthDate.getDate()){
+        return 'Happy birthday!'
+    }
+    let nextBirthDate = new Date(today.getFullYear(),birthDate.getMonth(),birthDate.getDate())
+    if(nextBirthDate<today){
+        nextBirthDate = new Date(today.getFullYear()+1,birthDate.getMonth(),birthDate.getDate())
+    }
+    return `Your birthday will be on ${days[nextBirthDate.getDay()]} ${months[nextBirthDate.getMonth()]} ${nextBirthDate.getDate()}, ${nextBirthDate.getFullYear()}`
+}
+function parseDate(str) {
+    const mdy = str.split('-');
+    return new Date(mdy[0], mdy[1]-1, mdy[2]);
+}

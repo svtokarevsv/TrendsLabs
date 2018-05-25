e0f5aa1b2e090dd41099470fcf50c466ec7c8be7
Sviatoslav Tokariev <svtokarevsv@gmail.com>
2018-05-25 18:26:16 -0400

    lab3 final

--- lab3/js/script.js
+++ lab3/js/script.js
@@ -1,5 +1,46 @@
+let map;
+let center;
+let coolLocations;
+let geoCoder;
+let indexForMarkers = 0;
+let markers = [];
+class MapMarker {
+    constructor(address, latlng) {
+        this.Address = address;
+        this.LatLong = latlng;
+    }
+}
 function initMap() {
-    let center = { lat: 43.6532, lng: -79.38 };
-    var map = new google.maps.Map(document.getElementById('map'), { zoom: 12, center: center });
+    center = { lat: 43.6532, lng: -79.38 };
+    map = new google.maps.Map(document.getElementById('map'), { zoom: 12, center: center });
     // var marker = new google.maps.Marker({position: uluru, map: map});
+    geoCoder = new google.maps.Geocoder();
+    fetch('./js/locations.json?v=1.00')
+        .then(data => data.json())
+        .then(showLocations);
+}
+function createMarkerForLocation() {
+    geoCoder.geocode({ address: 'Toronto ' + coolLocations[indexForMarkers].address }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            let coolLocationCoords = results[0].geometry.location;
+            //i don't use these MapMarker but since it was in a task, I do create them
+            markers.push(new MapMarker(coolLocations[indexForMarkers].address, { lat: coolLocationCoords.lat(), lng: coolLocationCoords.lng() }));
+            var marker = new google.maps.Marker({
+                map: map,
+                position: coolLocationCoords
+            });
+            if (indexForMarkers === coolLocations.length - 1)
+                return;
+            indexForMarkers++;
+            createMarkerForLocation();
+        }
+        else {
+            setTimeout(createMarkerForLocation, 2000);
+        }
+    });
+}
+function showLocations(locations) {
+    coolLocations = locations;
+    console.log(coolLocations);
+    createMarkerForLocation();
 }
--- lab3/js/script.ts
+++ lab3/js/script.ts
@@ -1,8 +1,58 @@
-import {LatLng}  from './LatLng.js';
+import {LatLng} from './LatLng'
+
+let map: any
+let center: LatLng
+let coolLocations: object[]
+let geoCoder: any
+let indexForMarkers: number = 0
+let markers: MapMarker[] = []
+
+class MapMarker {
+    public Address: string
+    public LatLong: LatLng
+
+    public constructor(address: string, latlng: LatLng) {
+        this.Address = address
+        this.LatLong = latlng
+    }
+}
 
 function initMap() {
-    let center:LatLng={ lat: 43.6532, lng: -79.38};
-    var map = new google.maps.Map(
+    center = {lat: 43.6532, lng: -79.38};
+    map = new google.maps.Map(
         document.getElementById('map'), {zoom: 12, center: center});
     // var marker = new google.maps.Marker({position: uluru, map: map});
+    geoCoder = new google.maps.Geocoder();
+    fetch('./js/locations.json?v=1.00')
+        .then(data => data.json())
+        .then(showLocations)
+}
+
+function createMarkerForLocation() {
+    geoCoder.geocode({address: 'Toronto ' + coolLocations[indexForMarkers].address}, function (results: any, status: string) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            let coolLocationCoords = results[0].geometry.location
+            //i don't use these MapMarker but since it was in a task, I do create them
+            markers.push(new MapMarker(
+                coolLocations[indexForMarkers].address,
+                {lat: coolLocationCoords.lat(), lng: coolLocationCoords.lng()}
+            ))
+            var marker = new google.maps.Marker(
+                {
+                    map: map,
+                    position: coolLocationCoords
+                });
+            if (indexForMarkers === coolLocations.length - 1) return
+            indexForMarkers++
+            createMarkerForLocation()
+        } else {
+            setTimeout(createMarkerForLocation, 2000)
+        }
+    });
+}
+
+function showLocations(locations: object[]) {
+    coolLocations = locations
+    console.log(coolLocations);
+    createMarkerForLocation()
 }
